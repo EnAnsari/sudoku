5d0078d6c1b806d8a9912140aef68f0fd8951ade
EnAnsari <rahmat2022b@gmail.com>
2022-08-13 19:48:41 +0430

    first method of sudoku slover by python

---
+++ method_1/main.py
@@ -0,0 +1,83 @@
+# powered by Rahmat
+# github: https://github.com/EnAnsari
+# Email: Rahmat2022a@gmail.com
+
+N = 9
+
+
+def printing(arr):
+    for i in range(N):
+        for j in range(N):
+            print(arr[i][j], end=" ")
+        print()
+
+
+def isSafe(grid, row, col, num):
+    for x in range(9):
+        if grid[row][x] == num:
+            return False
+
+    for x in range(9):
+        if grid[x][col] == num:
+            return False
+
+    startRow = row - row % 3
+    startCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if grid[i + startRow][j + startCol] == num:
+                return False
+    return True
+
+
+def solveSudoku(grid, row, col):
+    if (row == N - 1 and col == N):
+        return True
+    if col == N:
+        row += 1
+        col = 0
+    if grid[row][col] > 0:
+        return solveSudoku(grid, row, col + 1)
+    for num in range(1, N + 1, 1):
+        if isSafe(grid, row, col, num):
+            grid[row][col] = num
+
+            if solveSudoku(grid, row, col + 1):
+                return True
+
+        grid[row][col] = 0
+    return False
+
+
+print('Enter a 9*9 Matrix with 0 instead of unknown numbers:')
+grid = [list(map(int, input().split())) for i in range(N)]
+
+print('\nResult:')
+if (solveSudoku(grid, 0, 0)):
+    printing(grid)
+else:
+    print("no solution exists ")
+
+# Example:
+# input:
+# 3 0 6 5 0 8 4 0 0
+# 5 2 0 0 0 0 0 0 0
+# 0 8 7 0 0 0 0 3 1
+# 0 0 3 0 1 0 0 8 0
+# 9 0 0 8 6 3 0 0 5
+# 0 5 0 0 9 0 6 0 0
+# 1 3 0 0 0 0 2 5 0
+# 0 0 0 0 0 0 0 7 4
+# 0 0 5 2 0 6 3 0 0
+# output:
+# 3 1 6 5 7 8 4 9 2
+# 5 2 9 1 3 4 7 6 8
+# 4 8 7 6 2 9 5 3 1
+# 2 6 3 4 1 5 9 8 7
+# 9 7 4 8 6 3 1 2 5
+# 8 5 1 7 9 2 6 4 3
+# 1 3 8 9 4 7 2 5 6
+# 6 9 2 3 5 1 8 7 4
+# 7 4 5 2 8 6 3 1 9
+
+# Enjoy the code!
